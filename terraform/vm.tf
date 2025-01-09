136cfd8c5d806bbe10d2f61eeac7a1d94bb051ef
johnydev-1 <gitlab.imply909@simplelogin.com>
2025-01-09 18:02:30 -0500

    upgrade vm size for nexus CPU requirements

--- terraform/vm.tf
+++ terraform/vm.tf
@@ -4,7 +4,7 @@ resource "azurerm_virtual_machine" "nexus" {
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.nic.id]
-  vm_size               = "Standard_B2s"
+  vm_size               = "Standard_B4ms"
 
   storage_image_reference {
     publisher = "sonatypeinc1724257499617"
