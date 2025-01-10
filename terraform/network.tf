6e51dbaebda7849ccfb7b2f910b03c5c3a4c0eaa
johnydev-1 <gitlab.imply909@simplelogin.com>
2025-01-09 23:24:39 -0500

    allow port 8082 on azure nexus vm

--- terraform/network.tf
+++ terraform/network.tf
@@ -63,4 +63,16 @@ resource "azurerm_network_security_group" "sg" {
     source_address_prefix      = "0.0.0.0/0"
     destination_address_prefix = "*"
   }
+
+  security_rule {
+    name                       = "Allow-Nexus-8082"
+    priority                   = 1020
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8082"
+    source_address_prefix      = "0.0.0.0/0"
+    destination_address_prefix = "*"
+  }
 }
